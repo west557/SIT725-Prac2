94783ba3b11a97436354e2a7881e7d8cacadd179
Scott West <west55721@gmail.com>
2021-03-30 21:14:13 +1100

    added merged code from bootstrap

--- test.js
+++ test.js
@@ -1,2 +1,19 @@
-// This is a test file. 
-console.log("I love Software Engineering!");
+// This is a test file.
+
+let express = require("express");
+let app = express();
+
+let http = require('http').createServer(app);
+
+var port = process.env.port || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/test", function (request, response) {
+    var user_name = request.query.user_name;
+    response.end("Hello " + user_name + "!");
+})
+
+http.listen(port,()=>{
+    console.log("listening on port ", port);
+});
